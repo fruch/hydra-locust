5c963350ae15f1d7f02262c664d9aad76a623b90
Israel Fruchter <fruch@scylladb.com>
2020-02-03 10:07:50 +0200

    add prepared statments to cql case

--- locustfile.py
+++ locustfile.py
@@ -5,6 +5,7 @@ from locust import TaskSet, between, task
 
 import prom_collector  # pylint: disable=unused-import
 from common import CqlLocust, report_timings_cql, iter_zipf, iter_shuffle
+from cassandra import ConsistencyLevel
 
 KEYS = cycle(iter_shuffle(range(1, 10000)))
 READ = iter_zipf(10000, 2.0, num_samples=100)
@@ -14,6 +15,8 @@ class CqlTaskSet(TaskSet):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.session = None
+        self.insert_stmt = None
+        self.read_stmt = None
 
     def on_start(self):
         self.session = self.client.connect()
@@ -29,19 +32,17 @@ class CqlTaskSet(TaskSet):
         """)
         self.session.execute("USE keyspace1")
 
+        self.insert_stmt = self.session.prepare("INSERT INTO standard1 (key, C0) VALUES (?, ?)")
+        self.insert_stmt.consistency_level = ConsistencyLevel.QUORUM
+        self.read_stmt = self.session.prepare("SELECT * FROM standard1 WHERE key=?")
+        self.read_stmt.consistency_level = ConsistencyLevel.QUORUM
+
     @report_timings_cql
     @task(10)
     def insert(self):
         key = next(KEYS)
         np.random.seed(key)
-
-        self.session.execute(
-            """
-            INSERT INTO standard1 (key, C0)
-            VALUES (%s, %s)
-            """,
-            (key.to_bytes(10, byteorder='big'), np.random.bytes(64))
-        )
+        self.session.execute(self.insert_stmt, (key.to_bytes(10, byteorder='big'), np.random.bytes(64)))
 
     @report_timings_cql
     @task(5)
@@ -49,9 +50,7 @@ class CqlTaskSet(TaskSet):
         key = next(READ)
         np.random.seed(key)
         data = np.random.bytes(64)
-        res = self.session.execute(
-            "SELECT * FROM standard1 WHERE key=%s", (int(key).to_bytes(10, byteorder='big'), )
-        )
+        res = self.session.execute(self.read_stmt, (int(key).to_bytes(10, byteorder='big'), ))
         assert res.one().c0 == data, f"key={int(key)} data validation failed"
 
 
