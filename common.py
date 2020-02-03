2d6893700b02527380c65ac9c8c152fee903b92d
Israel Fruchter <fruch@scylladb.com>
2020-02-03 09:35:24 +0200

    add iter_zipf() and iter_suffle()

--- common.py
+++ common.py
@@ -2,7 +2,9 @@ import logging
 import sys
 import time
 from functools import wraps
+import random
 
+import numpy as np
 import boto3
 import botocore
 from locust import Locust, events
@@ -71,3 +73,51 @@ def report_timings_dynamodb(func):
             events.request_success.fire(request_type="dynamodb", name=func.__name__,
                                         response_time=total_time, response_length=0)
     return wrapper
+
+
+def iter_shuffle(iterable, bufsize=1000):
+    """
+    Shuffle an iterator. This works by holding `bufsize` items back
+    and yielding them sometime later. This is NOT 100% random, proved or anything.
+    idea from https://gist.github.com/andres-erbsen/1307752
+    """
+    iterable = iter(iterable)
+    buf = []
+    try:
+        while True:
+            for _ in range(random.randint(1, bufsize-len(buf))):
+                buf.append(next(iterable))
+            random.shuffle(buf)
+            for _ in range(random.randint(1, bufsize)):
+                if buf:
+                    yield buf.pop()
+                else:
+                    break
+    except StopIteration:
+        random.shuffle(buf)
+        while buf:
+            yield buf.pop()
+            return
+
+
+def iter_zipf(n, alpha, num_samples):
+    # idea from https://stackoverflow.com/q/31027739/459189
+
+    # Calculate Zeta values from 1 to n:
+    tmp = np.power(np.arange(1, n+1), -alpha)
+    zeta = np.r_[0.0, np.cumsum(tmp)]
+    # Store the translation map:
+    dist_map = [x / zeta[-1] for x in zeta]
+
+    while True:
+        # Generate an array of uniform 0-1 pseudo-random values:
+        u = np.random.random_sample(num_samples)
+        # bisect them with dist_map
+        v = np.searchsorted(dist_map, u)
+        for t in v:
+            yield t
+
+
+if __name__ == "__main__":
+    for i in iter_zipf(10000000, 2.0, 10000):
+        print(i, end=" ")
